8330236321a688f07303ba71e01832f0db5b84a1
ljw12334 <ljw12334@g.shingu.ac.kr>
2023-09-20 15:24:07 +0900

    week04 menu

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class PokemonGame {
     public static void main(String[] args) {
@@ -6,17 +7,48 @@ public class PokemonGame {
 //        System.out.println((int)(Math.random() * 6) + 1);
         int enemyPick = (int)(Math.random() * 3);
 
+        Pokemon enemyPokemon;
+        Pokemon myPokemon;
+
         if (enemyPick == 0) {
             NoFly noFly = new NoFly();
-            Pikachu pikachu = new Pikachu(noFly);
+            enemyPokemon = new Pikachu(noFly);
         } else if (enemyPick == 1) {
             NoFly noFly = new NoFly();
-            Squirtle squirtle = new Squirtle(noFly);
+            enemyPokemon = new Squirtle(noFly);
         } else if (enemyPick == 2) {
             Wings wings = new Wings();
-            Charizard charizard = new Charizard(wings);
+            enemyPokemon = new Charizard(wings);
         } else {
+            System.out.println("실행되면안됨");
+        }
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("포켓몬을 고르세요\n1)피카츄   2)꼬부기   3)리자몽 : ");
+        int pokemonPick = scanner.nextInt();
+        if (pokemonPick == 1) {
+            myPokemon = new Pikachu(new NoFly());
+        } else if (pokemonPick == 2) {
+            myPokemon = new Squirtle(new NoFly());
+        } else if (pokemonPick == 3) {
+            myPokemon = new Charizard(new Wings());
+        } else {
+            System.out.println("정상적인 값이 아닙니다!1");
+        }
+
+        int menu;
+        while (true) {
+            System.out.print("\n1)전투   2)도망   3)종료 : ");
+            menu = scanner.nextInt();
+
+            if (menu == 1) {
+
+            } else if (menu == 2) {
+
+            } else {
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
         }
     }
 }
