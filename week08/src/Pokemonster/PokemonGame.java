220a908f8e7cd6c93168636a8e54a4b526451d77
ljw12334 <ljw12334@g.shingu.ac.kr>
2023-10-18 15:22:07 +0900

    week08 v41

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -9,43 +9,52 @@ public class PokemonGame {
 //        System.out.println(Math.random());
 //        System.out.println((int)(Math.random() * 6) + 1);
         int enemyPick = (int)(Math.random() * 3);
-
-        try {
-            Pokemon enemyPokemon = null;
-            Pokemon myPokemon = null;          //추상클래스 변수선언 가능
+        Pokemon enemyPokemon = null;
+        Pokemon myPokemon = null;          //추상클래스 변수선언 가능
 //            Pokemonster.Pokemon pokemon = new Pokemonster.Pokemon(); //추상클래스의 객체 생성은 불가
 
-            if (enemyPick == 0) {
-                NoFly noFly = new NoFly();
-                enemyPokemon = new Pikachu(noFly);
-            } else if (enemyPick == 1) {
-                NoFly noFly = new NoFly();
-                enemyPokemon = new Squirtle(noFly);
-            } else if (enemyPick == 2) {
-                Wings wings = new Wings();
-                enemyPokemon = new Charizard(wings);
-            } else {
-                System.out.println("실행되면안됨");
-            }
+        if (enemyPick == 0) {
+            NoFly noFly = new NoFly();
+            enemyPokemon = new Pikachu(noFly);
+        } else if (enemyPick == 1) {
+            NoFly noFly = new NoFly();
+            enemyPokemon = new Squirtle(noFly);
+        } else if (enemyPick == 2) {
+            Wings wings = new Wings();
+            enemyPokemon = new Charizard(wings);
+        } else {
+            System.out.println("실행되면안됨");
+        }
 
-            enemyPokemon.name = "상대 " + enemyPokemon.name;
+        enemyPokemon.name = "상대 " + enemyPokemon.name;
 
+        try {
             Scanner scanner = new Scanner(System.in);
 
             while (true) {
-                System.out.print("포켓몬을 고르세요\n1)피카츄   2)꼬부기   3)리자몽 : ");
-                int pokemonPick = scanner.nextInt();
-                if (pokemonPick == 1) {
-                    myPokemon = new Pikachu(new NoFly());
-                    break;
-                } else if (pokemonPick == 2) {
-                    myPokemon = new Squirtle(new NoFly());
-                    break;
-                } else if (pokemonPick == 3) {
-                    myPokemon = new Charizard(new Wings());
-                    break;
-                } else {
-                    System.out.println("정상적인 값이 아닙니다!");
+                try {
+                    System.out.print("포켓몬을 고르세요\n1)피카츄   2)꼬부기   3)리자몽 : ");
+                    int pokemonPick = scanner.nextInt();
+                    if (pokemonPick == 1) {
+                        myPokemon = new Pikachu(new NoFly());
+                        break;
+                    } else if (pokemonPick == 2) {
+                        myPokemon = new Squirtle(new NoFly());
+                        break;
+                    } else if (pokemonPick == 3) {
+                        myPokemon = new Charizard(new Wings());
+                        break;
+                    } else {
+                        System.out.println("정상적인 값이 아닙니다!");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("입력 값은 숫자여야 합니다.");
+                    System.out.println("예외 내용 : " + e.getMessage());
+                    scanner.nextLine();
+                } catch (Exception e) {
+                    System.out.println("예외 발생");
+                    System.out.println("예외 내용 : " + e.getMessage());
+                    scanner.nextLine();
                 }
             }
 
@@ -58,28 +67,31 @@ public class PokemonGame {
                     while (true) {
                         System.out.print("기술 : 1)" + myPokemon.skills.get(0) + "   2)" + myPokemon.skills.get(1) + "   3)" + myPokemon.skills.get(2) + "   4)" + myPokemon.skills.get(3) + " : ");
                         skillNumber = scanner.nextInt();
-                        if (skillNumber >= 1 && skillNumber <= 4) {
+                        if (skillNumber >= 1 && skillNumber <= myPokemon.skills.size()) {
                             myPokemon.attack(enemyPokemon, skillNumber);
+                            enemyPokemon.attack(myPokemon, (int) (Math.random() * enemyPokemon.skills.size()) + 1);
                             break;
                         } else {
-                            System.out.println("기술은 1 ~ 4번 까지만 존재합니다!\n\n");
+                            System.out.println("메뉴에서 선택하세요");
                         }
                     }
-                    enemyPokemon.attack(myPokemon, (int) (Math.random() * 4) + 1);
-
                 } else if (menu == 2) {
 
-                } else {
+                } else if (menu == 3) {
                     System.out.println("무사히 도망쳤다!");
                     break;
+                } else {
+                    System.out.println("메뉴에서 선택하세요");
                 }
-
             }
         } catch (InputMismatchException e) {
             System.out.println("입력 값은 숫자여야 합니다.");
             System.out.println("예외 내용 : " + e.getMessage());
         } catch (NullPointerException e) {
             System.out.println("객체가 생성되지 않았습니다.");
+            System.out.println("예외 내용 : " + e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("선택 범위를 벗어났습니다.");
             System.out.println("예외 내용 : " + e.getMessage());
         } catch (Exception e) {
             System.out.println("예외가 발생하였습니다.");
