08db4ece580a438af6102bd23b87f3ba8cd022c2
ljw12334 <ljw12334@g.shingu.ac.kr>
2023-11-08 15:08:14 +0900

    week11 v47

---
+++ week11/src/Fly/Flyable.java
@@ -0,0 +1,15 @@
+package Fly;
+
+public interface Flyable {
+//    public Fly.Flyable(){
+//
+//    }
+
+    //Pokemonster.Pokemon pokemon;
+
+    void fly(); // 추상 메서드
+
+//    default void fly(){ // 구현 코드가 있는 디폴트 메서드
+//        System.out.println("날아 갑니다!");
+//    }
+}
